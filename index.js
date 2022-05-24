82d12ceb09577b6149aa968a28afa3ffe465ca72
Your Name <shubm@gmail.com>
2022-05-24 13:23:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const form = require('./model/models')
+
+
+
+
+
+// create express app
+const app = express();
+// Setup server port
+const port = process.env.PORT || 4000;
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+
+
+
+// define a root/default route
+app.get('/', (req, res) => {
+  res.json({"message": "Hello World"});
+});
+
+// Connecting to DB
+mongoose.connect('mongodb://localhost:27017/subform', {useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => {
+ console.log('connected to db')
+}).catch((error) => {
+ console.log(error)
+})
+
+// Adding a form
+app.post('/', (req, res) => {
+    let { name, email, phone,place  } = req.body
+    if (name && email && phone ){
+
+
+    name = req.body.name,
+    email = req.body.email,
+    phone = req.body.phone,
+    place = req.body.place
+   let newForm = new form({
+     name: name,
+     email: email,
+     phone: phone,
+     place: place
+    })
+    newForm.save().then((form) => {
+     res.send(form)
+    }).catch((err) => {
+     console.log(error)
+    })
+}
+   })
+
+
+
+
+
+
+
+
+// listen for requests
+app.listen(port, () => {
+  console.log(`Node server is listening on port ${port}`);
+});
---
+++ model/models.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+
+
+// Schema for form
+const formSchema = mongoose.Schema({
+    name: {
+     type: String,
+     required: true
+    },
+    email: {
+     type: String,
+     required: true,
+    lowercase: true },
+    
+    phone: {
+     type: Number,
+     required: true
+    },
+    place: {
+     type: String,
+     required: true
+    }
+   })
+   //Creating the collection Address
+   const form = mongoose.model('form', formSchema)
+
+   module.exports = form;
